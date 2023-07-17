88571cbb39398cd22aa88e678769904be0d649db
Jayanth <jayanthambaldhage15@gmail.com>
2023-07-17 15:55:22 +0530

    Incorporate PokemonManager and ViewModel to get JSON data print successfully in the console.

---
+++ PokeInfo/Managers/PokemonManager.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonManager.swift
+//  PokeInfo
+//
+//  Created by Jayanth Ambaldhage on 17/07/23.
+//
+
+import Foundation
+
+class PokemonManager {
+    func getPokemon() -> [Pokemon] {
+        let data: PokemonPage = Bundle.main.decode(file: "pokemon.json")
+        let pokemon: [Pokemon] = data.results
+        return pokemon
+    }
+    
+    func getDetailedPokemon(id: Int, _ completion:@escaping (DetailPokemon)->()){
+        Bundle.main.fetchData(url: "https://pokeapi.co/api/v2/pokemon/\(id)/", model: DetailPokemon.self) {
+            data in completion(data)
+        } failure: { error in
+            print(error)
+        }
+    }
+}
