c57729c311ed8affac9984bae0ffe90879db01e7
Jayanth <jayanthambaldhage15@gmail.com>
2023-07-17 14:39:43 +0530

    Add helpers to decode static JSON data and JSON data from internet.

---
+++ PokeInfo/Helpers/Helpers.swift
@@ -0,0 +1,46 @@
+//
+//  Helpers.swift
+//  PokeInfo
+//
+//  Created by Jayanth Ambaldhage on 17/07/23.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode<T:Decodable>(file:String)-> T{
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Could not find \(file) in bundle.")
+        }
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Could not load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        guard let loadedData = try? decoder.decode(T.self, from: data) else {
+            fatalError("Could not load \(file) from bundle.")
+        }
+        return loadedData
+    }
+    
+    func fetchData<T:Decodable>(url: String, model: T.Type, completion:@escaping(T)->(), failure:@escaping(Error)->()) {
+        guard let url = URL(string: url) else {return}
+        URLSession.shared.dataTask(with: url) {(data,response,error) in
+            guard let data = data else {
+                if let error = error {
+                    failure(error)
+                }
+                return
+            }
+            
+            do {
+                let safeData = try JSONDecoder().decode(T.self, from: data)
+                completion(safeData)
+            } catch {
+                failure(error)
+            }
+            
+        }
+        .resume()
+    }
+}
