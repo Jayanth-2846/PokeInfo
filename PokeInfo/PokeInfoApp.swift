93c926fc8e3bd78c0446900210bdcd16980ead5b
Jayanth <jayanthambaldhage15@gmail.com>
2023-07-17 13:04:55 +0530

    Initial Commit

---
+++ PokeInfo/PokeInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeInfoApp.swift
+//  PokeInfo
+//
+//  Created by Jayanth Ambaldhage on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
